8c256ed86015b7a0168dcd71076200dda486fa49
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2022-11-10 15:51:54 +0100

    added json control

--- src/script.js
+++ src/script.js
@@ -1,67 +1,47 @@
 const cols = document.querySelectorAll(".col");
 
-let array = [
-    {
-        day: "mon",
-        amount: 17.45,
-    },
-    {
-        day: "tue",
-        amount: 34.91,
-    },
-    {
-        day: "wed",
-        amount: 52.36,
-    },
-    {
-        day: "thu",
-        amount: 31.07,
-    },
-    {
-        day: "fri",
-        amount: 23.39,
-    },
-    {
-        day: "sat",
-        amount: 43.28,
-    },
-    {
-        day: "sun",
-        amount: 25.48,
-    },
-];
+const requestURL = "../src/data.json";
 
-const createInfo = function (e) {
-    const parentCol = e.target;
+async function populate() {
+    const request = new Request(requestURL);
 
-    const newTag = document.createElement("span");
-    if (parentCol.className == "monday col") {
-        newTag.innerHTML = `${array[0].amount}`;
-    } else if (parentCol.className == "tuesday col") {
-        newTag.innerHTML = `${array[1].amount}`;
-    } else if (parentCol.className == "wednesday col") {
-        newTag.innerHTML = `${array[2].amount}`;
-    } else if (parentCol.className == "thursday col") {
-        newTag.innerHTML = `${array[3].amount}`;
-    } else if (parentCol.className == "friday col") {
-        newTag.innerHTML = `${array[4].amount}`;
-    } else if (parentCol.className == "saturday col") {
-        newTag.innerHTML = `${array[5].amount}`;
-    } else if (parentCol.className == "sunday col") {
-        newTag.innerHTML = `${array[6].amount}`;
-    }
-    newTag.className = "col-description";
+    const response = await fetch(request);
+    const data = await response.json();
 
-    parentCol.appendChild(newTag);
-};
+    const createInfo = function (e) {
+        const parentCol = e.target;
 
-const removeInfo = function (e) {
-    const tag = e.target.querySelector(".col-description");
-    if (tag) {
-        tag.remove();
-    }
-};
+        const newTag = document.createElement("span");
+        if (parentCol.className == "monday col") {
+            newTag.innerHTML = `${data[0].amount}`;
+        } else if (parentCol.className == "tuesday col") {
+            newTag.innerHTML = `${data[1].amount}`;
+        } else if (parentCol.className == "wednesday col") {
+            newTag.innerHTML = `${data[2].amount}`;
+        } else if (parentCol.className == "thursday col") {
+            newTag.innerHTML = `${data[3].amount}`;
+        } else if (parentCol.className == "friday col") {
+            newTag.innerHTML = `${data[4].amount}`;
+        } else if (parentCol.className == "saturday col") {
+            newTag.innerHTML = `${data[5].amount}`;
+        } else if (parentCol.className == "sunday col") {
+            newTag.innerHTML = `${data[6].amount}`;
+        }
+        newTag.className = "col-description";
 
-cols.forEach((col) => col.addEventListener("mouseover", createInfo));
+        parentCol.appendChild(newTag);
+    };
 
-cols.forEach((col) => col.addEventListener("mouseleave", removeInfo));
+    const removeInfo = function (e) {
+        const tag = e.target.querySelector(".col-description");
+        if (tag) {
+            tag.remove();
+        }
+    };
+
+    cols.forEach((col) => col.addEventListener("mouseover", createInfo));
+
+    cols.forEach((col) => col.addEventListener("mouseleave", removeInfo));
+}
+
+populate();
